1db23b323d990f87195460a4885a87c8a92e1740
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-03-30 17:55:34 +0700

    LAB04.2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -19,8 +19,6 @@ app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
