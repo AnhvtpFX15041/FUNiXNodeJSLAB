e4d68059fed5b2cdf779917f92f2f777fac4b114
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-03-30 14:06:43 +0700

    LAB03.9

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expresshbs = require('express-handlebars');
+
 
 const app = express();
-app.engine('hbs', expresshbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
-const { ExpressHandlebars } = require('express-handlebars');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
