9847fb6b93119d49e6ca1858a2d59c4519b7a83e
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-04-04 12:20:17 +0700

    LAB05.1

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'Tendaydu1993'
+});
+
+module.exports = pool.promise();
