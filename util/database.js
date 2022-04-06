1493b55be476268048771d9a936f1149a99bb5ea
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-04-06 22:26:42 +0700

    LAB06.1

--- util/database.js
+++ util/database.js
@@ -1,8 +1,14 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'Tendaydu1993', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://phuonganh:funix2021@cluster0.n2j3y.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client => {
+        console.log('Connected');
+        callback(client);
+    })
+    .catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
+
