40f8bd8e1ed1ffd10bdfb175aa63356bf230e6c3
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-05-16 22:17:17 +0700

    LAB06.17

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
     User.findById('6266bce774bc58728da7a5f1').then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => console.log(err));
