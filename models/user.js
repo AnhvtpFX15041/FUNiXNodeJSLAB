ff1a9467bcc20fb034808ca0b38b40318115b871
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-05-17 22:55:07 +0700

    LAB06.23

--- models/user.js
+++ models/user.js
@@ -68,7 +68,17 @@ class User {
 
     addOrder(){
         const db = getDb();
-        return db.collection('orders').insertOne(this.cart).then(result => {
+        return this.getCart().then(products => {
+            const order = {
+                items: products,
+                user: {
+                    _id: new ObjectId(this._id),
+                    name: this.name
+                }
+            };
+            return db.collection('orders').insertOne(order);
+        })
+        .then(result => {
             this.cart = {item: []};
             return db
             .collection('users')
@@ -76,6 +86,11 @@ class User {
             {$set: {cart: {items: []}}}
         );
         })
+    }
+
+    getOrders(){
+        const db = getDb();
+        // return db.collection('orders').
     }
 
     static findById(userId){
