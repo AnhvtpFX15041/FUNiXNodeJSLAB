bb4fdbd624df0750e43d4e9341094307733c18ce
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-05-18 22:20:37 +0700

    LAB07.1

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -33,8 +32,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() =>{
+mongoose.connect('mongodb+srv://phuonganh:funix2021@cluster0.n2j3y.mongodb.net/test?retryWrites=true&w=majority').then(result => {
     app.listen(3000);
+}).catch(err => {
+    console.log(err);
 })
 
 
