8b084a182792f8bf7efa5f97ffa25e741ece6557
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-05-22 12:10:30 +0700

    LAB07.8

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//     User.findById('6266bce774bc58728da7a5f1').then(user => {
-//         req.user = new User(user.name, user.email, user.cart, user._id);
-//         next();
-//     })
-//     .catch(err => console.log(err));
+app.use((req, res, next) => {
+    User.findById('6289c4908bf9ad1afed6b7c5').then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => console.log(err));
     
-// });
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -33,6 +33,18 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://phuonganh:funix2021@cluster0.n2j3y.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+    User.findOne().then(user => {
+        if (!user){
+            const user = new User({
+                name: 'Alex',
+                email: 'alex@gmail.com',
+                cart: {
+                    items: []
+                }
+            });
+            user.save();
+        }
+    })
     app.listen(3000);
 }).catch(err => {
     console.log(err);
