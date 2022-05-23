12694bcb3ec5de1d1de175b4ebea96c7d6bbc4b4
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-05-23 22:29:15 +0700

    LAB07.16

--- models/user.js
+++ models/user.js
@@ -44,6 +44,11 @@ userSchema.methods.removeFromCart = function(productId){
     this.cart.items = updatedCartItems;
     return this.save();
 }
+
+userSchema.methods.clearCart = function(){
+    this.cart = {items: []};
+    return this.save();
+}
 module.exports = mongoose.model('User', userSchema);
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
