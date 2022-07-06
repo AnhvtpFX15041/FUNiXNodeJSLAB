cbef75b75233c4498d94afdde760a0d99ffd3708
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-07-06 23:15:11 +0700

    LAB08.24

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,9 +15,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
+    errorMessage: message,
   });
 };
 
@@ -41,6 +48,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
+          req.flash("error", "Invalid email or password");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -58,6 +66,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash(
+          "error",
+          "E-mail exists already, please pick a different one."
+        );
         return res.redirect("/signup");
       }
       return bcrypt
