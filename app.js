95730f739cfd4427523a0a5d0d9d8a7eaf8cfafb
Vu Thi Phuong Anh <anhvtpFX15041@funix.edu.vn>
2022-07-25 11:19:44 +0700

    LAB09.1

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const csrf = require("csurf");
 const flash = require("connect-flash");
+
 const errorController = require("./controllers/error");
 const User = require("./models/user");
+
 const MONGODB_URI =
   "mongodb+srv://phuonganh:funix2021@cluster0.n2j3y.mongodb.net/shop";
+
 const app = express();
 const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: "sessions",
 });
 const csrfProtection = csrf();
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -36,6 +40,7 @@ app.use(
 );
 app.use(csrfProtection);
 app.use(flash());
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
@@ -47,14 +52,17 @@ app.use((req, res, next) => {
     })
     .catch((err) => console.log(err));
 });
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
 app.use(errorController.get404);
 
 mongoose
